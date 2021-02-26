cfd5ccc1cc5b81fb389f7c3a82fbe220e26eed53
Imola <imolci01@gmail.com>
2021-02-26 08:48:34 +0200

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Labor1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labor1.dir/main.c.obj"
+  "Labor1.exe"
+  "Labor1.exe.manifest"
+  "Labor1.pdb"
+  "libLabor1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Labor1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int ossz(int,int);
+
+int main() {
+    printf("%d",ossz(5,3));
+    return 0;
+}
+int ossz(int a,int b)
+{
+    return a+b;
+}
